db7a7d9f2561418c63a9207384b5026d51270d32
Geoffrey Challen <challen@illinois.edu>
2019-04-19 16:08:19 -0500

    Adding some examples.

---
+++ examples/Adder.java
@@ -0,0 +1,9 @@
+public final class Adder {
+    /*
+     * Probably the easiest possible example.
+     */
+    @Solution
+    public static int add(int first, int second) {
+        return first + second;
+    }
+}
---
+++ examples/Printer.java
@@ -0,0 +1,10 @@
+public final class Printer {
+    /*
+     * Because this function is static and void answerable should determine that it must produce output. So record
+     * both System.out and System.err and compare against the solution.
+     */
+    @Solution
+    static void printHello() {
+        System.out.println("Hello, world!");
+    }
+}
