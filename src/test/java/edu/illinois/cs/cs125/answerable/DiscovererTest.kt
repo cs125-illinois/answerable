e786c5377006f9a629996ad42ad56510d40fb065
JKTKops <zerglingk9012@gmail.com>
2019-05-30 15:24:03 -0500

    All tests pass

--- src/test/java/edu/illinois/cs/cs125/answerable/DiscovererTest.kt
+++ src/test/java/edu/illinois/cs/cs125/answerable/DiscovererTest.kt
@@ -7,18 +7,14 @@ import java.lang.IllegalStateException
 import java.lang.reflect.Field
 
 internal class DiscovererTest {
-    private val correctAdderReference: String = "examples.adder.correct1.reference.Adder"
-    private val correctAdderAttempt: String = "examples.adder.correct1.Adder"
-    private val correctPrinterReference: String = "examples.printer.correct1.reference.Printer"
+    private val correctAdderReference: String = "examples.adder.correct.reference.Adder"
+    private val correctAdderAttempt: String = "examples.adder.correct.Adder"
+    private val correctPrinterReference: String = "examples.printer.correct.reference.Printer"
     private val correctClassDesignReference: String = "examples.classdesign.correct1.reference.ClassDesign"
 
     @Test
     fun testGetSolutionClassAdder() {
-        try {
-            getSolutionClass(correctAdderReference)
-        } catch (e: IllegalStateException) {
-            assertTrue(false)
-        }
+        getSolutionClass(correctAdderReference)
     }
 
     @Test
@@ -32,80 +28,56 @@ internal class DiscovererTest {
 
     @Test
     fun testGetAttemptClassAdder() {
-        try {
-            getAttemptClass(correctAdderAttempt)
-        } catch (e: IllegalStateException) {
-            assertTrue(false)
-        }
+        getAttemptClass(correctAdderAttempt)
     }
 
     @Test
     fun testGetReferenceSolutionMethod() {
-        try {
-            val soln = getSolutionClass(correctAdderReference)
-            val ref = soln.getReferenceSolutionMethod()
-            assertEquals("add", ref.name)
-        } catch (unused: Exception) {
-            assertTrue(false)
-        }
+        val soln = getSolutionClass(correctAdderReference)
+        val ref = soln.getReferenceSolutionMethod()
+        assertEquals("add", ref.name)
     }
 
     @Test
     fun testGetSolutionAttemptMethod() {
-        try {
-            val soln = getSolutionClass(correctAdderReference)
-            val solnMethod = soln.getReferenceSolutionMethod()
+        val soln = getSolutionClass(correctAdderReference)
+        val solnMethod = soln.getReferenceSolutionMethod()
 
-            val attempt = getAttemptClass(correctAdderAttempt)
-            val attemptMethod = attempt.findSolutionAttemptMethod(solnMethod)
+        val attempt = getAttemptClass(correctAdderAttempt)
+        val attemptMethod = attempt.findSolutionAttemptMethod(solnMethod)
 
-            assertEquals("add", attemptMethod.name)
-            assertEquals(Int::class.java, attemptMethod.genericReturnType)
-            assertArrayEquals(arrayOf(Int::class.java, Int::class.java), attemptMethod.genericParameterTypes)
-        } catch (unused: Exception) {
-            assertTrue(false)
-        }
+        assertEquals("add", attemptMethod.name)
+        assertEquals(Int::class.java, attemptMethod.genericReturnType)
+        assertArrayEquals(arrayOf(Int::class.java, Int::class.java), attemptMethod.genericParameterTypes)
     }
 
     @Test
     fun testIsStaticVoid() {
-        try {
-            val printerReferenceClass = getSolutionClass(correctPrinterReference)
-            val printerReferenceMethod = printerReferenceClass.getReferenceSolutionMethod()
-            assertTrue(printerReferenceMethod.isStaticVoid())
-        } catch (unused: Exception) {
-            assertTrue(false)
-        }
+        val printerReferenceClass = getSolutionClass(correctPrinterReference)
+        val printerReferenceMethod = printerReferenceClass.getReferenceSolutionMethod()
+        assertTrue(printerReferenceMethod.isStaticVoid())
     }
 
     @Test
     fun testGetPublicFields() {
-        try {
-            val classDesignReferenceClass = getSolutionClass(correctClassDesignReference)
-            assertEquals(
-                "[public static int examples.classdesign.correct1.reference.ClassDesign.numGets]",
-                classDesignReferenceClass.getPublicFields().toString()
-            )
-        } catch (unused: Exception) {
-            assertTrue(false)
-        }
+        val classDesignReferenceClass = getSolutionClass(correctClassDesignReference)
+        assertEquals(
+            "[public static int examples.classdesign.correct1.reference.ClassDesign.numGets]",
+            classDesignReferenceClass.getPublicFields().toString()
+        )
     }
 
     @Test
     fun testGetPublicMethods() {
-        try {
-            val classDesignReferenceClass = getSolutionClass(correctClassDesignReference)
-            assertEquals(
-                "[public java.lang.Object examples.classdesign.correct1.reference.ClassDesign.get(int)]",
-                classDesignReferenceClass.getPublicMethods(isReference = true).toString()
-            )
+        val classDesignReferenceClass = getSolutionClass(correctClassDesignReference)
+        assertEquals(
+            "[public java.lang.Object examples.classdesign.correct1.reference.ClassDesign.get(int)]",
+            classDesignReferenceClass.getPublicMethods(isReference = true).toString()
+        )
 
-            assertEquals(
-                "[public java.lang.Object examples.classdesign.correct1.reference.ClassDesign.get(int), public static examples.classdesign.correct1.reference.ClassDesign examples.classdesign.correct1.reference.ClassDesign.next]",
-                classDesignReferenceClass.getPublicMethods(isReference = false).toString()
-            )
-        } catch (unused: Exception) {
-            assertTrue(false)
-        }
+        assertEquals(
+            "[public java.lang.Object examples.classdesign.correct1.reference.ClassDesign.get(int), public static examples.classdesign.correct1.reference.ClassDesign examples.classdesign.correct1.reference.ClassDesign.next(examples.classdesign.correct1.reference.ClassDesign,int)]",
+            classDesignReferenceClass.getPublicMethods(isReference = false).toString()
+        )
     }
 }
