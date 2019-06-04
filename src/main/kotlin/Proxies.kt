bf74e7523a0ad74663f24308c9b538c6891b27f5
Ben N <ben@rigeltechnical.com>
2019-06-04 14:07:10 -0500

    Changes from Max

--- src/main/kotlin/Proxies.kt
+++ src/main/kotlin/Proxies.kt
@@ -3,16 +3,33 @@ package edu.illinois.cs.cs125.answerable
 import javassist.util.proxy.Proxy
 import javassist.util.proxy.ProxyFactory
 import org.objenesis.ObjenesisStd
+import org.objenesis.instantiator.ObjectInstantiator
+
+val objenesis = ObjenesisStd()
+
+/*
+ * For performance reasons, we want to re-use instantiators as much as possible.
+ * A map is used for future-safety so that as many proxy instantiators as are needed can be created safely,
+ * even if using only one is the most common use case.
+ *
+ * We map from 'superClass' instead of directly from 'proxyClass' as we won't have access to
+ * the same reference to 'proxyClass' on future calls.
+ */
+val proxyInstantiators: MutableMap<Class<*>, ObjectInstantiator<out Any?>> = mutableMapOf()
 
 fun mkProxy(superClass: Class<*>, childClass: Class<*>, forward: Any?): Any {
-    val factory = ProxyFactory()
-    val objenesis = ObjenesisStd()
+    // if we don't have an instantiator for this proxy class, make a new one
+    val instantiator = proxyInstantiators[superClass] ?: run {
+        val factory = ProxyFactory()
 
-    factory.superclass = superClass
-    factory.setFilter { it.name != "finalize" }
-    val proxyClass = factory.createClass()
+        factory.superclass = superClass
+        factory.setFilter { it.name != "finalize" }
+        val proxyClass = factory.createClass()
 
-    val instantiator = objenesis.getInstantiatorOf(proxyClass)
+        val newInstantiator = objenesis.getInstantiatorOf(proxyClass)
+        proxyInstantiators[superClass] = newInstantiator
+        newInstantiator
+    }
     val subProxy = instantiator.newInstance()
 
     (subProxy as Proxy).setHandler { _, method, _, args ->
