7eb2add4cd1cab6a59776a5db1cd13fc46eae5f2
Ben N <ben@rigeltechnical.com>
2019-06-05 09:50:46 -0500

    Make sure all TestSteps passed

--- src/test/java/edu/illinois/cs/cs125/answerable/ProxyingTest.kt
+++ src/test/java/edu/illinois/cs/cs125/answerable/ProxyingTest.kt
@@ -5,28 +5,29 @@ import org.junit.jupiter.api.Test
 
 internal class ProxyingTest {
 
+    fun assertAllSucceeded(results: Collection<TestStep>) {
+        results.forEach {
+            Assertions.assertNull(it.assertErr)
+            Assertions.assertTrue(it.succeeded)
+        }
+    }
+
     @Test
     fun testCorrectWidget() {
         val tg = TestGenerator(examples.proxy.reference.Widget::class.java, examples.proxy.Widget::class.java)
-        val results = tg.runTests(0x0403)
-        Assertions.assertTrue(results[0].succeeded)
-        Assertions.assertNull(results[0].assertErr)
+        assertAllSucceeded(tg.runTests(0x0403))
     }
 
     @Test
     fun testCorrectFieldWidget() {
         val tg = TestGenerator(examples.proxy.reference.FieldWidget::class.java, examples.proxy.FieldWidget::class.java)
-        val results = tg.runTests(0x0403)
-        Assertions.assertTrue(results[0].succeeded)
-        Assertions.assertNull(results[0].assertErr)
+        assertAllSucceeded(tg.runTests(0x0403))
     }
 
     @Test
     fun testGeneratedWidget() {
         val tg = TestGenerator(examples.proxy.reference.GeneratedWidget::class.java, examples.proxy.GeneratedWidget::class.java)
-        val results = tg.runTests(0x0403)
-        Assertions.assertTrue(results[0].succeeded)
-        Assertions.assertNull(results[0].assertErr)
+        assertAllSucceeded(tg.runTests(0x0403))
     }
 
 }
