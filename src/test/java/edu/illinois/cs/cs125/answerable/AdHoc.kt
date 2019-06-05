9e533b648ab9fabe2518050cde5b9a195097c9b1
JKTKops <zerglingk9012@gmail.com>
2019-06-05 16:28:12 -0500

    No-op'd AdHoc testing

--- src/test/java/edu/illinois/cs/cs125/answerable/AdHoc.kt
+++ src/test/java/edu/illinois/cs/cs125/answerable/AdHoc.kt
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test
 internal class AdHoc {
     @Test
     fun test() {
-        TestGenerator(examples.testing.reference.Test::class.java, examples.testing.Test::class.java).runTests(0x0403)
         // Nothing under ad-hoc testing right now
     }
 }
