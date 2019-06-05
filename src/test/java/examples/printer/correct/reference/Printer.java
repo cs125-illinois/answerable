37874e1f52d3dfaa29eafc88797ac294a4da144d
Ben N <ben@rigeltechnical.com>
2019-06-05 16:31:07 -0500

    Fix printer tests

--- src/test/java/examples/printer/correct/reference/Printer.java
+++ src/test/java/examples/printer/correct/reference/Printer.java
@@ -5,7 +5,7 @@ import edu.illinois.cs.cs125.answerable.Solution;
 public final class Printer {
      /* Because this function is static and void answerable should determine that it must produce output. So record
      * both System.out and System.err and compare against the solution. */
-    @Solution
+    @Solution(prints = true)
     static void printHello() {
         System.out.println("Hello, world!");
     }
