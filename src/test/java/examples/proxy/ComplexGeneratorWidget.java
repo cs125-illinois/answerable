199f06b54e5dbd6c12702e1e890cf7992c914093
Ben N <ben@rigeltechnical.com>
2019-06-05 14:31:08 -0500

    Write test that breaks StackMap

---
+++ src/test/java/examples/proxy/ComplexGeneratorWidget.java
@@ -0,0 +1,36 @@
+package examples.proxy;
+
+public class ComplexGeneratorWidget {
+
+    private String[] springs;
+
+    public String springSuffix = "";
+
+    public ComplexGeneratorWidget(int setSprings) {
+        springs = new String[setSprings];
+    }
+
+    public void createSprings(String prefix) {
+        for (int i = 0; i < springs.length; i++) {
+            springs[i] = prefix + i;
+        }
+    }
+
+    public void applySuffix() {
+        // Uses a public field so we can test proxying
+        for (int i = 0; i < springs.length; i++) {
+            springs[i] += springSuffix;
+        }
+    }
+
+    public String replaceSpring(int index, String newValue) {
+        String original = springs[index];
+        springs[index] = newValue;
+        return original;
+    }
+
+    public String getSpring(int index) {
+        return springs[index];
+    }
+
+}
