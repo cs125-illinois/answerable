24794f6de6fd6eec0e689853c970198c36640504
Ben N <ben@rigeltechnical.com>
2019-06-07 16:15:42 -0500

    Change bytecode verification exception

--- src/main/kotlin/TypeManagement.kt
+++ src/main/kotlin/TypeManagement.kt
@@ -196,7 +196,7 @@ internal fun verifyMemberAccess(referenceClass: Class<*>) {
     verifyMemberAccess(referenceClass, referenceClass, mutableSetOf(), mapOf())
 }
 
-private fun verifyMemberAccess(currentClass: Class<*>, referenceClass: Class<*>, checked: MutableSet<Class<*>>, dangerousAccessors: Map<String, AnswerableVerifyException>) {
+private fun verifyMemberAccess(currentClass: Class<*>, referenceClass: Class<*>, checked: MutableSet<Class<*>>, dangerousAccessors: Map<String, AnswerableBytecodeVerificationException>) {
     if (checked.contains(currentClass)) return
     checked.add(currentClass)
 
@@ -235,7 +235,7 @@ private fun verifyMemberAccess(currentClass: Class<*>, referenceClass: Class<*>,
                     }
                     if (Modifier.isStatic(field.modifiers) && field.isAnnotationPresent(Helper::class.java)) return@eachInstr
                     if (!Modifier.isPublic(field.modifiers))
-                        throw AnswerableVerifyException(method.name, currentClass, field)
+                        throw AnswerableBytecodeVerificationException(method.name, currentClass, field)
                 } else if (instr is InvokeInstruction) {
                     referenceClass.declaredMethods.filter { dm ->
                         dm.name == signatureConstant.getName(constantPool)
@@ -243,8 +243,8 @@ private fun verifyMemberAccess(currentClass: Class<*>, referenceClass: Class<*>,
                                 && Type.getSignature(dm) == signatureConstant.getSignature(constantPool)
                                 && (setOf(Generator::class.java, Next::class.java, Helper::class.java).none { dm.isAnnotationPresent(it) } || !Modifier.isStatic(dm.modifiers))
                     }.forEach { candidate ->
-                        dangerousAccessors[candidate.name]?.let { throw AnswerableVerifyException(it, method.name, currentClass) }
-                        if (!candidate.name.contains('$')) throw AnswerableVerifyException(method.name, currentClass, candidate)
+                        dangerousAccessors[candidate.name]?.let { throw AnswerableBytecodeVerificationException(it, method.name, currentClass) }
+                        if (!candidate.name.contains('$')) throw AnswerableBytecodeVerificationException(method.name, currentClass, candidate)
                     }
                 }
             } else if (checkInner) {
@@ -260,7 +260,7 @@ private fun verifyMemberAccess(currentClass: Class<*>, referenceClass: Class<*>,
         toCheck.methods.filter { it.name.contains('$') }.forEach {
             try {
                 checkMethod(it, false)
-            } catch (e: AnswerableVerifyException) {
+            } catch (e: AnswerableBytecodeVerificationException) {
                 dangersToInnerClasses.put(it.name, e)
             }
         }
@@ -269,17 +269,17 @@ private fun verifyMemberAccess(currentClass: Class<*>, referenceClass: Class<*>,
     methodsToCheck.forEach { checkMethod(it, true) }
 }
 
-class AnswerableVerifyException(val blameMethod: String, val blameClass: Class<*>, val member: Member) : AnswerableMisuseException("Illegal use of non-public submission members") {
+class AnswerableBytecodeVerificationException(val blameMethod: String, val blameClass: Class<*>, val member: Member) : AnswerableVerificationException("Bytecode error not specified. Please report a bug.") {
     override val message: String?
         get() {
             return "\nMirrorable method `$blameMethod' in `${blameClass.simpleName()}' " +
                     when (member) {
                         is java.lang.reflect.Method -> "calls non-public submission method: ${MethodData(member)}"
                         is Field -> "uses non-public submission field: ${member.name}"
-                        else -> throw IllegalStateException("AnswerableVerifyException.member must be a Method or Field. Please report a bug.")
+                        else -> throw IllegalStateException("AnswerableBytecodeVerificationException.member must be a Method or Field. Please report a bug.")
                     }
         }
-    constructor(fromInner: AnswerableVerifyException, blameMethod: String, blameClass: Class<*>) : this(blameMethod, blameClass, fromInner.member) {
+    constructor(fromInner: AnswerableBytecodeVerificationException, blameMethod: String, blameClass: Class<*>) : this(blameMethod, blameClass, fromInner.member) {
         initCause(fromInner)
     }
 }
