cd6dc36c7489263629b0db30630f0cf2d751523c
JKTKops <zerglingk9012@gmail.com>
2019-06-19 14:11:25 -0500

    Demo gatsby architecture is working

--- core/src/main/kotlin/api/BytecodeProvider.kt
+++ core/src/main/kotlin/api/BytecodeProvider.kt
@@ -1,21 +1,28 @@
-package edu.illinois.cs.cs125.answerable.api;
+package edu.illinois.cs.cs125.answerable.api
 
 // Written in Java so that Kotlin code can use it as a functional interface
 
 /**
  * An interface that allows Answerable to look up the bytecode for classes under test.
- * <p>
+ *
  * Applications must implement this interface if testing a class that does not exist on
  * disk in the application classpath.
  */
-public interface BytecodeProvider {
+@FunctionalInterface
+interface BytecodeProvider {
 
     /**
      * Looks up the bytecode used to create the class with [ClassLoader.defineClass].
+     *
      * If this provider is not responsible for [clazz], this method should throw [NoClassDefFoundError].
+     *
      * @param clazz a class under test
      * @return a byte array representing the compiled class
      */
-    byte[] getBytecode(Class<?> clazz);
+    fun getBytecode(clazz: Class<*>): ByteArray
 
+}
+
+fun bytecodeProvider(block: (Class<*>) -> ByteArray) = object : BytecodeProvider {
+    override fun getBytecode(clazz: Class<*>): ByteArray = block(clazz)
 }
--- demo/src/main/kotlin/edu/illinois/cs/cs125/answerablewebdemo/Main.kt
+++ demo/src/main/kotlin/edu/illinois/cs/cs125/answerablewebdemo/Main.kt
@@ -1,29 +1,22 @@
 package edu.illinois.cs.cs125.answerablewebdemo
 
-import edu.illinois.cs.cs125.answerable.api.Answerable
-import edu.illinois.cs.cs125.answerable.api.BytecodeProvider
-import io.github.cdimascio.dotenv.*
 
 import io.ktor.application.*
 import io.ktor.features.ContentNegotiation
-import io.ktor.http.*
 import io.ktor.jackson.jackson
 import io.ktor.request.receive
-import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import java.lang.IllegalStateException
 
-val dotenv = dotenv { directory = "demo/" }
-
 fun main() {
 
-    val server = embeddedServer(Netty, dotenv["ANSWERABLE_DEMO_PORT"]?.toInt()
-        ?: throw IllegalStateException("ANSWERABLE_DEMO_PORT not provided in .env")) {
+    val server = embeddedServer(Netty, System.getenv("ANSWERABLE_DEMO_PORT")?.toInt()
+        ?: throw IllegalStateException("ANSWERABLE_DEMO_PORT not provided.")) {
 
         install(ContentNegotiation) {
-            jackson { }
+            jackson()
         }
         routing {
             post("/") {
