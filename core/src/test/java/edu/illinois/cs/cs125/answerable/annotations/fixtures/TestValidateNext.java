aa956605eea93f4a64edce37b548c027956a769a
Geoffrey Challen <challen@illinois.edu>
2020-06-18 10:52:01 -0500

    Merging Max's changes.

--- core/src/test/java/edu/illinois/cs/cs125/answerable/annotations/fixtures/TestValidateNext.java
+++ core/src/test/java/edu/illinois/cs/cs125/answerable/annotations/fixtures/TestValidateNext.java
@@ -25,30 +25,32 @@ public class TestValidateNext {
   // missing `current` argument
   @Next
   public static TestValidateNext broken2(int complexity, Random random) {
-      return null;
+    return null;
   }
 
   // extra argument on the end
   @Next
-  public static TestValidateNext broken3(TestValidateNext o, int complexity, Random random, boolean b) {
-      return o;
+  public static TestValidateNext broken3(
+      TestValidateNext o, int complexity, Random random, boolean b) {
+    return o;
   }
 
   // extra arguments at the beginning
   @Next
-  public static TestValidateNext broken4(boolean b, TestValidateNext o, int complexity, Random random) {
-      return o;
+  public static TestValidateNext broken4(
+      boolean b, TestValidateNext o, int complexity, Random random) {
+    return o;
   }
 
   // return type is wrong
   @Next
   public static int broken5(TestValidateNext o, int complexity, Random random) {
-      return 0;
+    return 0;
   }
 
   // `current` type is wrong
   @Next
   public static TestValidateNext broken6(int i, int complexity, Random random) {
-      return null;
+    return null;
   }
 }
